4c96cc649d7cf734502e03595bfb166324f9b53e
hcarneiro <hcarneiro@weboo.biz>
2017-07-27 17:26:10 +0100

    Number field created

---
+++ js/components/number.js
@@ -0,0 +1,13 @@
+Fliplet.FormBuilder.field('number', {
+  name: 'Number input',
+  category: 'Text inputs',
+  props: {
+    fieldType: {
+      type: String,
+      default: 'number'
+    },
+    placeholder: {
+      type: String
+    }
+  }
+});
