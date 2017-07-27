a93d4317a0f2596f762995715f1319c115ebc5f3
hcarneiro <hcarneiro@weboo.biz>
2017-07-27 17:26:22 +0100

    Telephone field created

---
+++ js/components/telephone.js
@@ -0,0 +1,13 @@
+Fliplet.FormBuilder.field('telephone', {
+  name: 'Telephone input',
+  category: 'Text inputs',
+  props: {
+    fieldType: {
+      type: String,
+      default: 'tel'
+    },
+    placeholder: {
+      type: String
+    }
+  }
+});
