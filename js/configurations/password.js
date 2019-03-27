ffc83e6e546bbaf9c2f134bc1691cc73fc14932f
Nicholas Valbusa <squallstar@gmail.com>
2019-03-27 13:23:50 +0100

    alert for confirmation when autogenerate gets enabled

--- js/configurations/password.js
+++ js/configurations/password.js
@@ -1,1 +1,17 @@
-Fliplet.FormBuilder.configuration('password');
+Fliplet.FormBuilder.configuration('password', {
+  watch: {
+    autogenerate: function (val) {
+      if (!val) {
+        return;
+      }
+
+      if (this.confirm) {
+        Fliplet.Modal.alert({
+          message: 'Password confirmation and autogeneration can\'t both be enabled. Enabling one disables the other.'
+        });
+      }
+
+      this.confirm = false;
+    }
+  }
+});
