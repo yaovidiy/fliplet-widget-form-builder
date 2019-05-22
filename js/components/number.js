8023d4ea9972dff97ee5f9273218d44a1454462d
Nicholas Valbusa <squallstar@gmail.com>
2019-05-22 13:50:57 +0200

    refactor

--- js/components/number.js
+++ js/components/number.js
@@ -15,47 +15,21 @@ Fliplet.FormBuilder.field('number', {
     }
   },
   methods: {
-    verifyInput: function(event) {
-      if (!event) {
-        return;
-      }
+    updateValue: function () {
+      var $vm = this;
 
-      var code = event.which ? event.which : event.keyCode;
+      var rx = new RegExp(
+        this.positiveOnly ? '[^0-9\.,]' : '[^0-9\.,-]'
+      , 'g')
 
-      // Do not allow dot/comma characters if settings are not allowing decimals
-      if ([188, 190].indexOf(code) !== -1) {
-        if (parseInt(this.decimals, 10) === 0 || this.value.match(/\.,/)) {
-          event.preventDefault();
-        }
-
-        return;
-      }
-
-      // Allow minus sign only as first character given the input
-      // is allowing negative numbers.
-      if (code === 189) {
-        if (this.positiveOnly || this.value.length) {
-          event.preventDefault();
-        }
-
-        return;
-      }
-
-      // Allow 0-9 numbers
-      if (code >= 48 && code <= 57) {
-        return true;
-      }
-
-      // Allow modifiers keys, delete, return and arrows
-      if ([13, 16, 17, 18, 8, 37, 38, 39, 40, 91].indexOf(code) !== -1) {
-        return;
-      }
-
-      // Don't allow other inputs
-      event.preventDefault();
+      this.value = this.value.replace(rx, '');
+      this.$emit('_input', this.name, this.value);
     }
   },
   computed: {
+    inputType: function () {
+      return this.positiveOnly ? 'number' : 'text';
+    },
     pattern: function () {
       if (this.positiveOnly) {
         return '\\d*';
